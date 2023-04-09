c6372b2feeb32ab266e3a364055bd5c42422540f
Javier Bartús Gómez <91955122+Bartu10@users.noreply.github.com>
2023-04-09 22:27:07 +0200

    Delete src directory

--- src/config.js
+++
@@ -1,4 +0,0 @@
-export const PAYPAL_API_CLIENT = "ATkm4g3-rYmG-a63gELmXkL3tTbfXFxiERpsba2h2pqohrIhWT1DqcVwXWnh6DjBAY7GXio8aDk9MrCp"
-export const PAYPAL_API_SECRET = "EGnon332k63EHCOe_xcL62AIE45RsaFFBIWgYR1TMbpfnj0GRS6CRhvbVkSAYHF-jSoEk7PxDyCHViGw"
-export const PAYPAL_API = 'https://api-m.sandbox.paypal.com'
-
