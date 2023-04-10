c47bab4ea5be226612c73df0882e42983e960a2d
Javier Bartús Gómez <91955122+Bartu10@users.noreply.github.com>
2023-04-10 12:36:41 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import paymentRoutes from './routes/payment.routes'
-
-const app = express();
-
-app.use(paymentRoutes)
-
-app.listen(3000);
-console.log("server on port", 3000);
