b0aeb16406adc4480ee1ebf7b44adf29c45ae8cc
Bartu10 <jbg10102002@gmail.com>
2023-06-14 23:53:16 +0200

    Final Provisional

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import express from 'express'
 import paymentRoutes from './routes/payment.routes'
 
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
-
+app.use(cors());
 app.use(paymentRoutes)
 
 app.listen(3000);
